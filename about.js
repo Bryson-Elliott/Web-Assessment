9bf3a8f20f5db80de210798a4630e9cc2c8952a6
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-03 12:06:19 -0600

    Finished with core

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert ('It was a success!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+img.addEventListener('mouseover', () => {
+	alert ('You look awesome today!')
+	let img = 'https://images.unsplash.com/photo-1525538182201-02cd1909effb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cnViYmVyJTIwZHVja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
+})
+//I'm not sure here, I will move on and try to come back
---
+++ profile.js
@@ -0,0 +1,18 @@
+let colorButton = document.querySelector('#color');
+
+colorButton.addEventListener('click', () => {
+    alert ('BLUE!')
+})
+
+let placeButton = document.querySelector('#place');
+
+placeButton.addEventListener('click', () => {
+    alert ('Disneyland!')
+})
+
+let ritualButton = document.querySelector('#ritual');
+
+ritualButton.addEventListener('click', () => {
+    alert ('Drinking a Caprisun during lecture')
+})
+
