ce90cb5e4d72e5ece534d7beb0d6c7d00e18d84a
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-04 19:51:55 -0600

    Fixed image hover

--- about.js
+++ about.js
@@ -13,8 +13,8 @@ let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
 
-img.addEventListener('mouseover', () => {
+document.querySelector('img').addEventListener('mouseover', () => {
 	alert ('You look awesome today!')
-	let img = 'https://images.unsplash.com/photo-1525538182201-02cd1909effb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cnViYmVyJTIwZHVja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
+
 })
 //I'm not sure here, I will move on and try to come back
